acbcfae91980299b9a97cc6c6097b0ca766639b1
unknown <marcosesteves2005@gmail.com>
2026-03-23 22:23:27 -0300

    Service

---
+++ src/main/java/com/example/Base/service/ProjetoService.java
@@ -0,0 +1,32 @@
+package com.example.Base.service;
+
+import com.example.Base.entity.Projeto;
+import com.example.Base.repository.ProjetoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProjetoService {
+
+    @Autowired
+    private ProjetoRepository repository;
+
+    public Projeto salvar(Projeto projeto) {
+        return repository.save(projeto);
+    }
+
+    public List<Projeto> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<Projeto> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
